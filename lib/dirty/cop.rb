49f44ddab88e744c9b28796f18b7e7ae81efa643
melch <melch@procore.com>
2016-10-25 23:40:53 -0700

    Update dirty cop to work with rubocop 0.44. Delete specs since they're out of date and weren't used to dev. Remove some feature requests from the code - feature requests are now in github.

--- lib/dirty/cop.rb
+++ lib/dirty/cop.rb
@@ -6,30 +6,10 @@
 # having to restyle everything or adding cops incrementally. It relies
 # on git to figure out which files to check.
 #
-# Here are some options you can pass in addition to the ones in rubocop:
-#
-#   --local               Check only the changes you are about to push
-#                         to the remote repository.
-#
-#   --uncommitted         Check only changes in files that have not been
-#   --index               committed (i.e. either in working directory or
-#                         staged).
-#
-#   --against REFSPEC     Check changes since REFSPEC. This can be
-#                         anything that git will recognize.
-#
-#   --branch              Check only changes in the current branch.
-#
-#   --courage             Without this option, only the modified lines
-#                         are inspected. When supplied, it will check
-#                         the full contents of the file. You should have
-#                         the courage to fix your style violations as
-#                         you see them, you know.
-#
 # Caveat emptor:
 #
 # * Monkey patching ahead. This script relies on Rubocop internals and
-#   has been tested against 0.25.0. Newer (or older) versions might
+#   has been tested against 0.44 Newer (or older) versions might
 #   break it.
 #
 # * While it does try to check modified lines only, there might be some
@@ -40,95 +20,27 @@
 require 'rubocop'
 # require 'pry'
 
-module DirtyCop
-  extend self # In your face, style guide!
+class GitGitter
+  attr_reader :diff_info
 
-  def bury_evidence?(file, line)
-    !report_offense_at?(file, line)
+  def initialize
+    @diff_info = Hash[
+      changed_filenames.collect do |file|
+        [file, line_change_info(file)]
+      end
+    ]
   end
 
-  def ref
-    'HEAD'
-  end
-
-  def files_to_inspect(whitelisted_files, args)
-    return @files ||= args unless args.empty?
-
-    @files ||= (changed_files(ref) & whitelisted_files)
-  end
-
-  def cover_up_unmodified(ref, only_changed_lines = true)
-    @line_filter ||= changed_files_and_lines(ref) if only_changed_lines
-  end
-
-  def process_bribe
-    # leaving this unused method as a placeholder of flags that purportedly work
-    # (potential feature set)
-    only_changed_lines = true
-
-    # I am specifying the ref above instead of getting it from args since
-    # getting it from args seems to be broken
-    # ref = nil
-    # loop do
-    #   arg = ARGV.shift
-    #   case arg
-    #   when '--local'
-    #     ref = `git rev-parse --abbrev-ref --symbolic-full-name @{u}`.chomp
-    #     exit 1 unless $?.success?
-    #   when '--against'
-    #     ref = ARGV.shift
-    #   when '--uncommitted', '--index'
-    #     ref = 'HEAD'
-    #   when '--branch'
-    #     ref = `git merge-base HEAD master`.chomp
-    #   when '--courage'
-    #     only_changed_lines = false
-    #   else
-    #     ARGV.unshift arg
-    #     break
-    #   end
-    # end
-    # return unless ref
-
-    cover_up_unmodified ref, only_changed_lines
-  end
-
-  def report_offense_at?(file, line)
-    changed_lines_for_file(file).include? line
-  end
-
-  def changed_lines_for_file(file)
-    changed_files_and_lines(ref)[file] || []
-  end
-
-  def changed_files(ref)
-    @changed_files ||= git_diff_name_only
+  def changed_filenames
+    `git diff --diff-filter=AM --name-only HEAD`
       .lines
       .map(&:chomp)
       .grep(/\.rb$/)
       .map { |file| File.absolute_path(file) }
   end
 
-  def git_diff_name_only
-    `git diff --diff-filter=AM --name-only #{ref}`
-  end
-
-  def changed_files_and_lines(ref)
-    result = {}
-
-    changed_files(ref).each do |file|
-      result[file] = changed_lines(file, ref)
-    end
-
-    result
-  end
-
-  def git_diff(file, ref)
-    `git diff -p -U0 #{ref} #{file}`
-  end
-
-  def changed_lines(file, ref)
-    ranges = git_diff(file, ref)
+  def line_change_info(file)
+    line_change_info = `git diff -p -U0 HEAD #{file}`
       .each_line
       .grep(/@@ -(\d+)(?:,)?(\d+)? \+(\d+)(?:,)?(\d+)? @@/) {
         [
@@ -136,23 +48,39 @@ module DirtyCop
           (Regexp.last_match[4] || 1).to_i
         ]
       }.reverse
+  end
+end
 
-    mask = Set.new
+class DirtyCop
+  attr_reader :diff_info
 
-    ranges.each do |changed_line_number, number_of_changed_lines|
-      number_of_changed_lines.times do |line_delta|
-        mask << changed_line_number + line_delta
-      end
-    end
-
-    mask.to_a
+  def initialize(diff_info:)
+    @diff_info = diff_info
   end
 
-  def eat_a_donut
-    puts "#{$PROGRAM_NAME}: The dirty cop Alex Murphy could have been"
-    puts
-    puts File.read(__FILE__)[/(?:^#(?:[^!].*)?\n)+/s].gsub(/^#/, '   ')
-    exit
+  def changed_files_and_lines
+    # TODO: Make this work with a relative path filename passed in :/
+    @changes ||= Hash[
+      diff_info.collect do |filename, line_change_info|
+        mask = line_change_info.collect do |changed_line_number, number_of_changed_lines|
+          Array(changed_line_number .. (changed_line_number + number_of_changed_lines))
+        end.flatten
+
+        [filename, mask]
+      end
+    ]
+  end
+
+  def bury_evidence?(offense)
+    intersection = changed_files_and_lines[offense.location.source_buffer.name] & Array(offense.location.first_line..offense.location.last_line)
+    intersection && intersection.empty?
+  end
+
+  def files_to_inspect(whitelisted_files, args)
+    @files ||= args unless args.empty?
+
+    @files ||= (diff_info.keys & whitelisted_files)
+    @files
   end
 end
 
@@ -161,17 +89,21 @@ module RuboCop
     alias_method :find_unpatched, :find
 
     def find(args)
-      DirtyCop.files_to_inspect(find_unpatched(args), args)
+      # returns an array of full file paths that are the files to inspect.
+      DirtyCop.new(diff_info: GitGitter.new.diff_info).files_to_inspect(find_unpatched(args), args)
     end
   end
 
-  class Runner
-    alias_method :inspect_file_unpatched, :inspect_file
+  module Cop
+    class Commissioner
+      alias_method :straight_investigate, :investigate
 
-    def inspect_file(file)
-      offenses, updated = inspect_file_unpatched(file)
-      offenses = offenses.reject { |o| DirtyCop.bury_evidence?(file.path, o.line) }
-      [offenses, updated]
+      def investigate(processed_source)
+        # returns an array of offenses (Offense objects)
+        straight_investigate(processed_source).reject do |offense|
+          DirtyCop.new(diff_info: GitGitter.new.diff_info).bury_evidence?(offense)
+        end
+      end
     end
   end
 end
--- spec/dirty/cop_spec.rb
+++ spec/dirty/cop_spec.rb
@@ -1,101 +1,12 @@
 # encoding: utf-8
 
 require 'spec_helper'
+require 'suture'
 
-class RubocopTest
-  include DirtyCop
-end
+# class RubocopTest
+#   include DirtyCop
+# end
 
 describe DirtyCop do
   let(:rubocop) { RubocopTest.new }
-  let(:ref) { 'HEAD' }
-
-  describe '#files_to_inspect' do
-    context 'files passed in' do
-      it 'returns files passed into rubocop that have changes' do
-        files = ['company', 'user']
-        rubocop_allowed_files = ['dont_care']
-        result = rubocop.files_to_inspect(rubocop_allowed_files, files)
-        expect(result).to eq(files)
-      end
-    end
-
-    context 'no files passed in' do
-      it 'returns all files that have changes' do
-        changed_files = ['company', 'user']
-        rubocop_allowed_files = changed_files
-        rubocop.stub(:changed_files) { changed_files }
-
-        result = rubocop.files_to_inspect(rubocop_allowed_files, [])
-        expect(result).to eq(changed_files)
-      end
-
-      it 'returns files that have changes that are okayed by rubocop' do
-        changed_files = ['company', 'user', 'project']
-        rubocop_allowed_files = ['app', 'company', 'user']
-        rubocop.stub(:changed_files) { changed_files }
-
-        result = rubocop.files_to_inspect(rubocop_allowed_files, [])
-        expect(result).to eq(['company', 'user'])
-      end
-    end
-  end
-
-  describe '#changed_files' do
-    it 'returns an array of full path of files changed' do
-      rubocop.stub(:git_diff_name_only) { File.open('spec/diff_mocks/name_only') }
-      expected = [
-        "#{Dir.pwd}/lib/dirty/cop.rb",
-        "#{Dir.pwd}/spec/dirty/cop_spec.rb"
-      ]
-      expect(rubocop.changed_files(ref)).to eq(expected)
-    end
-  end
-
-  describe '#changed_lines' do
-    let(:file) { 'company' }
-
-    it 'returns an array, containing line numbers that have changed' do
-      rubocop.stub(:git_diff) { File.open('spec/diff_mocks/company') }
-      result = rubocop.changed_lines(file, ref)
-      expected = [4, 6, 7, 8, 9, 12]
-      expect(result.sort).to eq(expected)
-    end
-  end
-
-  describe '#report_offense_at?' do
-    it 'should respond if a line was changed at past file and line' do
-      changed_files_and_lines = {
-        'company' => [4, 6, 7, 8, 9, 12],
-        'user' => [2, 3, 6]
-      }
-      rubocop.stub(:changed_files_and_lines) { changed_files_and_lines }
-      expect(rubocop.report_offense_at?('company', 6)).to be_truthy
-      expect(rubocop.report_offense_at?('company', 1)).to be_falsy
-      expect(rubocop.report_offense_at?('other_file', 6)).to be_falsy
-    end
-
-    it 'should not blow up if no files were changed' do
-      changed_files_and_lines = {}
-      rubocop.stub(:changed_files_and_lines) { changed_files_and_lines }
-      expect(rubocop.report_offense_at?('company', 1)).to be_falsy
-    end
-  end
-
-  describe '#changed_files_and_lines' do
-    it 'should return a hash with filenames and changed lines' do
-      rubocop.stub(:changed_files) { %w(company user) }
-      rubocop.stub(:git_diff).with('company', 'HEAD') { File.open('spec/diff_mocks/company') }
-      rubocop.stub(:git_diff).with('user', 'HEAD') { File.open('spec/diff_mocks/user') }
-      expected = {
-        'company' => [4, 6, 7, 8, 9, 12],
-        'user' => [2, 3, 6]
-      }
-      result = rubocop.changed_files_and_lines(ref)
-      expect(result.keys).to eq(expected.keys)
-      result.keys.each do |key|
-        expect(result[key].sort).to eq(expected[key].sort)
-      end
-    end
-  end
 end
